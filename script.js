704574473ac3b1a7e14d9c880fd107ef749a39d9
Singh098890098890 <h.srawn@share.epsb.ca>
2024-12-20 11:45:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Average Calculator by Mr. V
+// Event Listener
+document.getElementById("calc-btn").addEventListener("click", calcAverage);
+// Event Function
+function calcAverage() {
+// Input
+let num1 = +document.getElementById("num1").value;
+let num2 = +document.getElementById("num2").value;
+let num3 = +document.getElementById("num3").value;
+// Process
+let average = (num1 + num2 + num3) / 3;
+// Output
+document.getElementById("ave-out").innerHTML = average;
+}
